050b6464548859cc4d12b681a02f8b3040fcded0
Taewoong <aipooh8882@naver.com>
2021-07-09 13:46:45 +0900

    setting webpack

---
+++ lecture/2.끝말잇기/WordRelay.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class WordRelay extends React.Component {
+    state = {
+
+    };
+
+    render() {
+
+    }
+}
+
+moduel.exports = WordRelay;
--- lecture/2.끝말잇기/webpack.config.js
+++ lecture/2.끝말잇기/webpack.config.js
@@ -1,3 +1,14 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실 서비스에서는: production
+    devtool: 'eval',  // 빠르게 하겠다는 의미
+
+    entry: {
+
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'),
+    }, // 출력
 };
