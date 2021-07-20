06a614985932c4ba4e79fc71815f2d2f694112db
Taewoong <aipooh8882@naver.com>
2021-07-20 16:51:27 +0900

    tried to admit react-refresh to it, but it doesn't work. gonna keep figure it out

---
+++ lecture/2 끝말잇기 with hooks/client.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const { hot } = require('react-hot-loader/root');
+
+
+// const WordRelay = require('./WordRelay');
+const WordRelay = require('./WordRelay');
+
+const Hot = hot(WordRelay);
+
+ReactDom.render(<Hot />, document.querySelector("#root"));
---
+++ lecture/2 끝말잇기 with hooks/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'word-relay-dev',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    entry: {
+        app: './client.jsx',
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel'
+                ],
+            },
+        }],
+    },
+    plugins: [
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist',
+    },
+};
