f2e7f48af43993ba123a28e9702bc6fd7bc55774
Taewoong <aipooh8882@naver.com>
2021-07-22 01:03:19 +0900

    숫자야구 completed

--- lecture/3. 숫자야구/NumberBaseball.jsx
+++ lecture/3. 숫자야구/NumberBaseball.jsx
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import Try from './Try';
 
 function getNumbers () { //숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
-
+    const candidate = [1,2,3,4,5,6,7,8,9,];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class NumberBaseball extends React.Component {
@@ -12,12 +19,55 @@ class NumberBaseball extends React.Component {
         tries: []
     };
 
-    onChangeInput = () => {
-
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+            });
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value: '',
+                answer: getNumbers(),
+                tries: [],
+            });
+        } else { // 답 틀렸으면
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) { // 10번 이상 틀렸을 때
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answer: getNumbers(),
+                    tries: [],
+                });
+            } else {
+                for (let i = 0; i < 4; i++) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball ++;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+                    value: '',
+                });
+            }
+        }
+        console.log(this.state.value);
     };
 
-    onSubmitForm = () => {
-
+    onChangeInput = (e) => {
+        console.log(this);
+        this.setState({
+            value: e.target.value,
+        });
     };
 
     render() {
@@ -29,11 +79,11 @@ class NumberBaseball extends React.Component {
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {['like', 'like', 'like', 'like', 'like'].map(() => {
+                    {this.state.tries.map((v, index) => { // v = try, result
                         return (
-                            <li>like</li>
+                            <Try key={`${index + 1}차 시도 :`} tryInfo={v} />
                         );
-                    })};
+                    })}
                 </ul>
             </>
         );
