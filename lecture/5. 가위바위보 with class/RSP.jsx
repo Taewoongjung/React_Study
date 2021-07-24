df22cde3a8146c5e6280a509082ad56890ff6ae3
Taewoong <aipooh8882@naver.com>
2021-07-24 14:39:29 +0900

    closure problem fixed

--- lecture/5. 가위바위보 with class/RSP.jsx
+++ lecture/5. 가위바위보 with class/RSP.jsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
 
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+};
+
+const scores = {
+    가위: 1,
+    바위: 0,
+    보: -1,
+};
+
 class RSP extends Component {
     state = {
         result: '',
-        imgCoord: 0,
+        imgCoord: '0',
         score: 0,
     };
 
-    onClickBtn() {
+    onClickBtn = (choice) => {
 
     }
 
+    interval;
+
+    componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이한다.
+        // const {imgCoord} = this.state; // 비동기 안에서 이렇게 바깥 함수를 참조하면 클로저 문제가 생긴다. 그래서 안에다가 해줘야 한다.
+        this.interval = setInterval(() => {
+            const {imgCoord} = this.state; // 이렇게 비동기 함수 안에다가 해줘야 한다.
+            if (imgCoord === rspCoords.바위) {
+                this.setState({
+                    imgCoord: rspCoords.가위,
+                });
+            } else if (imgCoord === rspCoords.가위) {
+                this.setState({
+                    imgCoord: rspCoords.보,
+                });
+            } else if (imgCoord === rspCoords.보) {
+                this.setState({
+                    imgCoord: rspCoords.바위,
+                });
+            }
+        }, 77);
+    }
+
+    componentDidUpdate() { // 리렌더링
+
+    }
+
+    componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 한다.
+        clearInterval(this.interval);
+    }
+
     render() {
-        const { result, score, imgCoor } = this.state;
+        const { result, score, imgCoord } = this.state;
         return (
             <>
-                <div id="computer" style={{ background: `url(https://en.ping.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+                <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
                 <div>
                     <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
                     <button id="scissor" className="btn" onClick={() => onClickBtn('가위')}>가위</button>
