0bb778f47ae06105db8d1aee9a1f0acf4ddb611d
Taewoong <aipooh8882@naver.com>
2021-07-25 20:25:19 +0900

    before css

--- lecture/6. lottery with class/Ball.jsx
+++ lecture/6. lottery with class/Ball.jsx
@@ -1,7 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class Ball extends PureComponent {
-    render() {
+// state를 사용하지 않는 애들임 hooks가 아님. 그냥 함수 컴포넌트라고 불림.
+// memo 또는 PureComponent로 감싸는게 hoc(high order component) 고차함수라고 불리운다.
+const Ball = memo(({ number }) => {
         const { number } = this.props;
         let background;
         if (number <= 10) {
@@ -15,8 +16,7 @@ class Ball extends PureComponent {
         }
         return (
             <div className="ball" style={{ background }}>{number}</div>
-        )
-    }
-}
+        );
+});
 
 export default Ball;
