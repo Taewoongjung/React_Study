bd5f81fd4378feaf648d795dc4440916f33ddb91
Taewoong <aipooh8882@naver.com>
2021-07-25 20:20:09 +0900

    lottery

---
+++ lecture/6. lottery with class/Lotto.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+    state = {
+        winNumbers: getWinNumbers(),
+        winBalls: [],
+        bonus: null,
+        redo: false,
+    };
+
+    onClickRedo() {
+
+    }
+
+    render() {
+        const { winBall, bonus, redo} = this.state;
+        return (
+            <>
+                <div>당첨 숫자</div>
+                <div id="결과창">
+                    {winBalls.map((v) => <Ball key={v} number={v} />)}
+                </div>
+                <div>보너스!</div>
+                {bonus && <Ball number={bonus} />}
+                <button onCLick={redo > this.onClickRedo : () => {}}>한 번 더!</button>
+            </>
+        );
+    }
+}
+
+export default Lotto;
