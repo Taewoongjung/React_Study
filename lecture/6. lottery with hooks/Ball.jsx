af1cd3ca9edded45ffcf9c22a438f7d2a1105444
Taewoong <aipooh8882@naver.com>
2021-07-26 19:28:24 +0900

    lottery with hooks

---
+++ lecture/6. lottery with hooks/Ball.jsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+
+// state를 사용하지 않는 애들임 hooks가 아님. 그냥 함수 컴포넌트라고 불림.
+// memo 또는 PureComponent로 감싸는게 hoc(high order component) 고차함수라고 불리운다.
+const Ball = memo(({ number }) => {
+        let background;
+        if (number <= 10) {
+            background = 'red';
+        } else if (number <= 20) {
+            background = 'orange';
+        } else if (number <= 40) {
+            background = 'blue';
+        } else {
+            background = 'green';
+        }
+        return (
+            <div className="ball" style={{ background }}>{number}</div>
+        );
+});
+
+export default Ball;
