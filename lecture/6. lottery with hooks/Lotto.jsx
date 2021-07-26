ee2862acfd3678328fbd456d58b2a3704eb0cfc9
Taewoong <aipooh8882@naver.com>
2021-07-26 21:53:17 +0900

    lottery with hooks completed

--- lecture/6. lottery with hooks/Lotto.jsx
+++ lecture/6. lottery with hooks/Lotto.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -13,80 +13,51 @@ function getWinNumbers() {
     return [...winNumbers, bonusNumber];
 }
 
-class Lotto extends Component {
-    state = {
-        winNumbers: getWinNumbers(), // 당첨 숫자들
-        winBalls: [],
-        bonus: null, // 보너스 공
-        redo: false,
-    };
+const Lotto = () => {
+    const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+    const [winBalls,setWinBalls] = useState([]);
+    const [bonus, setBonus] = useState(null);
+    const [redo,setRedo] = useState(false);
+    const timeouts = useRef([]);
 
-    timeouts = [];
-
-    runTimeouts = () => {
+    useEffect(() => {
         console.log('runTimeouts');
-        const { winNumbers } = this.state;
-        for (let i = 0; i < this.state.winNumbers.length - 1; i++) { // 비동기에다가 let을 쓰면 closure문제 안생긴다 || 보너스공을 빼야하니 -1을 함.
-            this.timeouts[i] = setTimeout(() => {
-                this.setState((prevState) => {
-                    return {
-                        winBalls: [...prevState.winBalls, winNumbers[i]],
-                    };
-                });
+        for (let i = 0; i < winNumbers.length - 1; i++) { // 비동기에다가 let을 쓰면 closure문제 안생긴다 || 보너스공을 빼야하니 -1을 함.
+            timeouts.current[i] = setTimeout(() => {
+                setWinBalls((prevState) => [...prevState, winNumbers[i]]);
             }, (i + 1) * 1000); // 1초마다 하나씩 추가된다.
         }
-        this.timeouts[6] = setTimeout(() => {
-            this.setState({
-                bonus:winNumbers[6],
-                redo: true,
+        timeouts.current[6] = setTimeout(() => {
+            setBonus(winNumbers[6]);
+            setRedo(true);
+        }, 7000);
+        return () => {
+            timeouts.current.forEach((v) => {
+                clearTimeout(v);
             });
-        }, 7000) // 마지막이 7초니깐 7초를 해준다.
+        };
+    }, [timeouts.current]); // deps가 빈 배열이면 componentDidMount랑 같다.
+    // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
+
+    const onClickRedo = () => {
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeouts.current = [];
     };
 
-    componentDidMount() {
-        console.log('componentDidMount');
-        this.runTimeouts();
-    }
-
-    // redo를 눌렀을 때 다시 돌려줘야 하니깐
-    componentDidUpdate(prevProps, prevState) { // 바뀌기 이전 state: prevState || 바뀐 후의 state: this.state
-        console.log('componentDidUpdate');
-        if (this.state.winBalls.length === 0) { // setState가 될 때 마다 componentDidUpdate가 실행되니깐 여기에서는 if문이 엄청 중요하다.
-            this.runTimeouts();
-        }
-    }
-
-    componentWillUnmount() {
-        console.log('componentWillUnmount');
-        this.timeouts.forEach((v) => {
-            clearTimeout(v);
-        });
-    }
-
-    onClickRedo = () => {
-        this.setState ({
-            winNumbers: getWinNumbers(), // 당첨 숫자들
-            winBalls: [],
-            bonus: null, // 보너스 공
-            redo: false,
-        });
-        this.timeouts = [];
-    };
-
-    render() {
-        const { winBalls, bonus, redo} = this.state;
-        return (
-            <>
-                <div>당첨 숫자</div>
-                <div id="결과창">
-                    {winBalls.map((v) => <Ball key={v} number={v} />)}
-                </div>
-                <div>보너스!</div>
-                {bonus && <Ball number={bonus} />}
-                {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
-            </>
-        );
-    }
+    return (
+        <>
+            <div>당첨 숫자</div>
+            <div id="결과창">
+                {winBalls.map((v) => <Ball key={v} number={v} />)}
+            </div>
+            <div>보너스!</div>
+            {bonus && <Ball number={bonus} />}
+            {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+        </>
+    );
 }
 
 export default Lotto;
